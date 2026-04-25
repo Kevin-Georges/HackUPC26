ff9050d6bd568c775bc6c1c73610877c924b4a9a
Ammaar <ammaar.ch@outlook.com>
2026-04-25 04:38:19 +0200

    initial_outline_of_md_compliant_script

---
+++ Subsystem_A_2.py
@@ -0,0 +1,22 @@
+###Recoating System,
+###Possible factors: Humidity/Contamination, Operational Load (over time), Maintenance
+###1) Work out current stress, multiply it by time its running for, and save to doc
+###2) Work out failure probability, that is also health indicator
+###3) Operational status is judged based on health index
+###4) Remember custom metrics for each subsystem
+###Abrasive Damage: Load, 
+print()
+
+def import_lifetime_load():
+
+
+
+
+def calculate_current_load():
+
+
+
+def main():
+    ###calls import_lifetime_load once
+
+    ###calls calculate_current_load every few seconds, outputs to graph
